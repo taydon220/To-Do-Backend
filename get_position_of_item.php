f6e66da8ee1cc63d74a9d808f6e00dbf05e9ad1c
taydon220 <taydon220@gmail.com>
2021-10-03 14:52:57 -0500

    Get position of given item

---
+++ get_position_of_item.php
@@ -0,0 +1,13 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    include('config.php');
+    $db = new SQLite3($DB_PATH);
+    $item_to_search = ucwords($_GET["item"]);
+    $statement = $db->prepare("SELECT position from items WHERE item = ?");
+    $statement->bindValue(1, $item_to_search);
+    $results = $statement->execute();
+    $results_array = $results->fetchArray(SQLITE3_ASSOC);
+    echo json_encode($results_array);
+}
+?>
