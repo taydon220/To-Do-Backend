5a146a7d0ebb488d034112fcae789fbd8d11cc81
taydon220 <taydon220@gmail.com>
2021-11-04 10:58:36 -0500

    Remove old files.

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-//Task position values.
-$NOTSTARTED = 'Not Started';
-$INPROGRESS = 'In Progress';
-$COMPLETED = 'Completed';
-
-$ACCEPTABLE_POSITIONS = array($NOTSTARTED, $INPROGRESS, $COMPLETED);
-?>
